f0a9d1e971381bdaee3e275eb1982e8c2bd6bab9
Dmytro Moskvin <dimamoskvin2002@gmail.com>
2021-04-21 19:57:03 +0300

    azure test

---
+++ ClientApp/src/webAPI/user.ts
@@ -0,0 +1,27 @@
+
+export const getUserData = async (token: string) => {
+	const query = JSON.stringify({
+		query: `{
+			getCurrentUser{
+				name
+				surname
+				email
+				position
+				department
+				computedProps {
+					permissions
+				}
+			}
+		}`
+	});
+  
+	return fetch('/graphql', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'Authorization': `Bearer ${token}`
+		},
+		body: query
+	})
+	.then(data => data.json());
+};
